4343aa1127edf95056a4bd6944e3330eaee221be
FANO <121119011+FANO-OFFICIAL@users.noreply.github.com>
2023-03-06 17:26:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const list = document.createElement('ul');
 const info = document.createElement('p');
 const html = document.querySelector('html');
 
-info.textContent = 'Below is a dynamic list. Click anywhere on the page to add a new list item. Click an existing list item to change its text to something else.';
+info.textContent = "Vous trouverez ci-dessous une liste dynamique. Cliquez n'importe où sur la page pour ajouter un nouvel élément de liste. Cliquez sur un élément de liste existant pour changer son texte en autre chose.";
 
 document.body.appendChild(info);
 document.body.appendChild(list);
